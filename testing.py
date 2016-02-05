59b1d4852b023a1e5dcd29052cb28082d26e3933
sebboyer <sebastienboyer54@gmail.com>
2016-02-05 10:26:49 -0500

    new version testing.py first commit

--- testing.py
+++ testing.py
@@ -19,176 +19,229 @@ from multiprocessing import *
 from functools import partial
 import copy
 
+import pickle
+
 
 ###############################################################################
 ####################### Test models and Networks ##############################
 ###############################################################################
-'''
-Test models in individual_models for all DataSet in X_test_list, y_test_list
-Args :  models, list of problems (target week, feature week), structure of network to use, list of X, list of y , list of models to skip in ['lr','svm','nn','rf']
-Returns : results[pb][target][source] contains the corresponding AUC on test for the given network of estimators
-'''
-def test_all_models(individual_models,X_test_list,y_test_list,network,DataSets=[],para=0,skip_models=[]):
-
-    if DataSets==[]:
-        DataSets=range(len(X_test_list))
-
-    ##### Initialize results
-    results={}
-    for target in DataSets: # For all possible target DataSets in DataSets
-        results[target]={}
-        potential_sources = [c for c in DataSets if c!=target]+['all']
-        for source in potential_sources: # For all remaning source as source including the aggregation of all
-            results[target][source] = {}
-
-    ##### Fill results
-    if para==1:
-        pool = Pool()
-        partial_test_model = partial(test_model,individual_models,DataSets,network,X_test_list,y_test_list,skip_models)
-        estimators_list = pool.map(partial_test_model, DataSets)
-    else:
-        estimators_list = []
-        for target in DataSets:
-            estimators_list.append(test_model(individual_models,DataSets,network,X_test_list,y_test_list,skip_models,target))
-    
-    # Transform list into dictionnary
-    for i,estimator_dic in enumerate(estimators_list):
-        results[i] = estimator_dic
-
-    return results
-
-def test_model(individual_models,DataSets,network,X_test_list,y_test_list,skip_models,target):
-
-    results = {}
-    X_test = X_test_list[target]
-    y_test = y_test_list[target]
-    potential_sources = [c for c in DataSets if c!=target]
-
-    networks = [copy.deepcopy(network) for i in range(len(potential_sources)+1)]
-
-    # Only Network built from one source
-    for j,source in enumerate(potential_sources): 
-        estimators = [individual_models[source][i] for i in individual_models[source] if i not in skip_models]
-        networks[j].layers[0].add_estimators(estimators)
-        results[source] = networks[j].evaluate(X_test,y_test)
-    
-    # Networks built from all individual models
-    estimators = [] # Networks built from all individual models
-    for source in potential_sources :
-        estimators.extend([individual_models[source][i] for i in individual_models[source] if i not in skip_models])
-    networks[-1].layers[0].add_estimators(estimators)
-    results['all'] = networks[-1].evaluate(X_test,y_test)
-
-    return results
-
-
 
 
 '''
-Args : results from test_all_models, list of problems to average over,  target and list of all sources
-Return : res[source][model] with the average AUC over the pbs in pb_list
-
+Apply network structure and return AUC on test (X_train/y_train to train potential meta-models)
 '''
-def contract_results(results,target,DataSets):
-
-    sources = [c for c in DataSets if c!=target]
-
-    ##### Initialize results
-    res = {}
-
-    ###### Fill with models from 1 source only
-
-    for source in sources: # Iterate over source sources
-        res[source]={}
-
-        res[source]['rf']=results[target][source]["Layer 0"]["Estimator 0"]
-        res[source]['lr']=results[target][source]["Layer 0"]["Estimator 1"]
-        res[source]['nn']=results[target][source]["Layer 0"]["Estimator 2"]
-        res[source]['vote simple']=results[target][source]["Layer 1"]["Estimator 0"]
-        res[source]['vote rank']=results[target][source]["Layer 1"]["Estimator 1"]
-        res[source]['vote norm']=results[target][source]["Layer 1"]["Estimator 2"]
-        res[source]['vote aggr']=results[target][source]["Layer 2"]["Estimator 0"]
-
-    ###### Fill with models from all ind models
-
-    source = 'all'
-    res[source]={}
-
-    res[source]['vote simple']=results[target][source]["Layer 1"]["Estimator 0"]
-    res[source]['vote rank']=results[target][source]["Layer 1"]["Estimator 1"]
-    res[source]['vote norm']=results[target][source]["Layer 1"]["Estimator 2"]
-    res[source]['vote aggr']=results[target][source]["Layer 2"]["Estimator 0"]
-
+def train_and_test(network,estimators,X_train,y_train,X_test,y_test):
+    network.layers[0].add_estimators(estimators)
+    network.train(X_train,y_train)
+    res = network.evaluate(X_test,y_test)
     return res
 
 '''
-Transfrom the contracted results from contract_results to a list 
-Args : contracted_results and list of sources
-Return : list of scores in order described in the function
-'''
-def contracted_result_toList(contracted_results,sources):
-    scores = []
-    sources = sources+['all']
-    models_ind = ['rf','lr','nn','vote simple','vote rank','vote norm','vote aggr']
-    models_all = ['vote simple','vote rank','vote norm','vote aggr']
+Apply network structure and return AUC for a particular target Dataset for:
+- meta models coming from each other sources individual-models
+- a meta model coming from the concatenated models
+- a meta model including all individual and concatenated models
 
-    for source in sources:
-        if source=='all':
-            models = models_all
-        else:
-            models = models_ind
-        for model in models:
-            scores.append(contracted_results[source][model])
+'''
+def cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test,y_test,X_val_list,y_val_list,skip_models,target):
+
+    results = {}
+    
+    potential_sources = [c for c in DataSets if c!=target]
+    X_train_concat = np.concatenate([X_test_list[source] for source in potential_sources])
+    y_train_concat = np.concatenate([y_test_list[source] for source in potential_sources])
+
+    networks = [copy.deepcopy(network) for i in range(len(potential_sources)+2)]
+
+    ########## Only Network built from one source
+    for j,source in enumerate(potential_sources): 
+        
+        estimators = [individual_models[source][i] for i in individual_models[source] if i not in skip_models] 
+        
+        X_train ,y_train = X_test_list[source],y_test_list[source]
+        results[source] = train_and_test(networks[j],estimators,X_train,y_train,X_test,y_test)
+        
+    ########## Networks built from concatenated data
+
+    estimators = [concat_models[target][i] for i in concat_models[target] if i not in skip_models]
+    results['concat'] = train_and_test(networks[-1],estimators,X_train_concat,y_train_concat,X_test,y_test)
+    
+    ########## Networks built from all individual models
+
+    estimators = [] 
+    for source in potential_sources :
+        estimators.extend([individual_models[source][i] for i in individual_models[source] if i not in skip_models])
+    results['all'] = train_and_test(networks[-2],estimators,X_train_concat,y_train_concat,X_test,y_test)
+
+    return results
+
+'''
+Apply cycle_train_and_test for each possible target as defined in DataSets
+return dictionnary of results 
+results[target][source]
+'''
+
+def test_models_pb(individual_models,concat_models,DataSets,X_test_list,y_test_list,X_train_list,y_train_list,folder,network_name,skip_models=[]):
+    
+    results ={}
+    
+    for target in DataSets:
+        print "# Target = ",target
+        
+        X_test = X_test_list[target]
+        y_test = y_test_list[target]
+        
+        X_val_list = [X_test_list[s] for s in DataSets if s!=target]
+        y_val_list = [y_test_list[s] for s in DataSets if s!=target]
+        
+        network = load_net(folder,network_name)
+        result_target = cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test,y_test,X_val_list,y_val_list,skip_models,target)
+        
+        results[target] = result_target
+        
+    return results
+
+
+
+
+
+'''
+Test all models (All pb + networks + target) 
+
+'''
+
+def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structure_folder,networks,concat_folder,para=0,skip_models=[],DataSets=[]):
+
+    if DataSets == []:
+        DataSets = range(len(feat_dic_list))
+    
+    results = {}
+    
+    def test_models_pb_aux(individual_models,feat_dic_list,featids_feat,structure_folder,networks,concat_folder,pb):
+        
+        print "########### Problem = ",pb
+
+        feat_weekids=[pb[1]]
+        label_weekid=pb[0]
+        
+        train_ratio=0.7
+        X_train_list,X_test_list,y_train_list,y_test_list=prepare_problem_data_all(feat_dic_list,featids_feat,feat_weekids,label_weekid,train_ratio)
+        
+        individual_models_pb = individual_models[pb]
+        concat_models_pb = pickle.load(open(concat_folder+"concat_model_("+str(pb[0])+", "+str(pb[1])+").p","rb"))
+        
+        result_pb = {}
+        for network_name in networks:
+            print "###### Testing network : "+network_name
+            result_pb[network_name] = test_models_pb(individual_models_pb,concat_models_pb,DataSets,X_test_list,y_test_list,X_train_list,y_train_list,structure_folder,network_name,skip_models)
             
-    scores = [x[0] if isinstance(x, list) else x for x in scores ]
+        return result_pb
+    
+    
+    ##### Fill results
+    if para==1:
+        partial_test_models_pb = partial(test_models_pb,individual_models,feat_dic_list,featids_feat,structure_folder,networks,concat_folder,pb)
+        pool = Pool()
+        results_dic = pool.map(partial_test_models_pb,pb_list)
+    else:
+        results_list = []
+        for pb in pb_list:
+            results_list.append(test_models_pb_aux(individual_models,feat_dic_list,featids_feat,structure_folder,networks,concat_folder,pb))
+        # Format results into dic
+        results = {}
+        for i,estimator_dic in enumerate(results_list):
+            results[pb_list[i]] = estimator_dic
+    
+    return results
 
-    return scores
+    
 
 
 '''
-Plot the list of scores using ggplot to compare performance of models
+Transform a results dictionnary of the form 
+results[pb][target][source][layer][estimator] (dictionnary)
+into a set of 3 lists :
+- scores 
+- indexes
+- colors
+for display and comparison purposes
 '''
-def ggplot_scores(scores,target,sources,problems_description):
+def transform_results(results,pb,target):
 
-    n_sources = len(sources)
+    c1 = "skyblue"
+    c2 = "royalblue"
+    c3 = "navy"
+    c4 = 'black'
 
-    # Build index (the list of x_ticks)
+    colors_list = [c1,c1,c1,c1,c2]
+
+    scores,index,colors = [],[],[]
+
     ind = 0
-    index = []
-    for i in range(n_sources):
-        index.extend(range(ind,ind+7))
-        ind = ind+8
-    index.extend(range(ind,ind+4))
+
+    results_exp = results['1L_simple'][target]
+    #### Add All models AUC
+    for source in results_exp:
+        if source != 'all': # For simple source
+            color_ind = 0
+            for layer in results_exp[source]:
+                for est in results_exp[source][layer]:
+                    scores.append(results_exp[source][layer][est])
+                    index.append(ind)
+                    colors.append(colors_list[color_ind])
+                    ind+=1
+                    color_ind+=1
+            ind+=1
+            
+    #### Add Network Full AUC
+    ind+=1
+    scores.append(results_exp['all']['Layer 1']['Estimator 0'])
+    index.append(ind)
+    colors.append(c3)
+
+    #### Add Network '1L_LR'
+    results_exp = results_networks['1L_LR'][target]
+    ind+=2
+    scores.append(results_exp['all']['Layer 1']['Estimator 0'])
+    index.append(ind)
+    colors.append(c3) 
+
+    #### Add Network '2L_LR'
+    results_exp = results_networks['2L_simple'][target]
+    ind+=2
+    scores.append(results_exp['all']['Layer 2']['Estimator 0'])
+    index.append(ind)
+    colors.append(c3)   
+
+    #### Add Network '2L_LR'
+    results_exp = results_networks['2L_LR'][target]
+    ind+=2
+    scores.append(results_exp['all']['Layer 2']['Estimator 0'])
+    index.append(ind)
+    colors.append(c3)  
+      
+    ## Formatting into pandas.DF
+    pretty_results ={'Scores':scores,'Index':index,'Color':colors}
+    data=pd.DataFrame.from_dict(pretty_results)
+
+    title = 'Performance of Classifiers'
+    xlabel = 'Light blue : simple classifiers  |  Medium blue : Vote inside one source  |  Navy blue : Ensembling networks'
 
     # Axis limits
     M_y = np.max(scores)+0.025
     M_x = np.max(index)+1
 
-    # Colors 
-    c1 = "skyblue"
-    c2 = "royalblue"
-    c3 = "navy"
-    one_source_colors = [c1]*3+[c2]*3+[c3]
-    Colors = one_source_colors*n_sources+[c2]*3+[c3]
-
-    # Text 
-    title = 'Performance of different predictive models on source '+str(target)+" for "+problems_description
-    xlabel = ["source "+str(x) for x in sources]+['All sources']
-    xlabel = '  |  '.join(xlabel)
-
-    # Formatting into pandas.DF
-    pretty_results ={'Scores':scores,'Index':index,'Color':Colors}
-    data=pd.DataFrame.from_dict(pretty_results)
-
     # PLotting with ggplot
     plot = ggplot(aes(x='Index',y='Scores'), data) + \
-     geom_bar(stat = 'identity',fill=Colors) + \
+     geom_bar(stat = 'identity',fill=colors) + \
     scale_y_continuous(limits = (0.5,M_y)) + \
     scale_x_continuous(limits = (-1,M_x)) + \
     ggtitle(title) + \
-    xlab(xlabel) + ylab('ROC-AUC score') 
+    xlab(xlabel) + ylab('ROC-AUC score')
 
     return plot
+
+
 
 
 
