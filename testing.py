a0b9366e7edab378a8c0c38e571bd72b90855b53
sebboyer <sebastienboyer54@gmail.com>
2016-04-04 22:58:54 -0400

    Fix issues with Dependencies

--- testing.py
+++ testing.py
@@ -2,26 +2,21 @@
 Author : Sebastien Boyer
 Date : January 2016
 
-Test models 
+Test models
 
 '''
 
 ############# Dependencies
 import sys
-sys.path.append('/Users/sebboyer/Documents/ALFA/data_copy/features')
-from dataFormat import *
-from visua import *
-from ensembling import *
-from create_network import *
-
 import numpy as np
 from ggplot import *
-
 from multiprocessing import *
 from functools import partial
 import copy
-
 import pickle
+
+from ensembling import *
+from create_network import *
 
 
 ###############################################################################
@@ -29,9 +24,15 @@ import pickle
 ###############################################################################
 
 class Ensemble:
-    def __init__(self,estimators,network_file):
+    def __init__(self,estimators,network=0,network_file=0):
         self.estimators=estimators
-        self.network = pickle.load(open(network_file,"rb"))
+        if network!=0:
+            self.network = network
+        else:
+            if network_file==0:
+                "Wrong network file. Please provide a network or a network file."
+            else:
+                self.network = pickle.load(open(network_file,"rb"))
 
     def train(self,X_train,y_train):
 
@@ -66,7 +67,7 @@ Apply network structure and return AUC for a particular target Dataset for:
 def cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test_list,y_test_list,include_est,skip_models,target):
 
     results = {}
-    
+
     potential_sources = [c for c in DataSets if c!=target]
     X_train_concat = np.concatenate([X_test_list[source] for source in potential_sources])
     y_train_concat = np.concatenate([y_test_list[source] for source in potential_sources])
@@ -77,21 +78,21 @@ def cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test
 
     ########## Only Network built from one source
     if 'unique_source' in include_est :
-        for j,source in enumerate(potential_sources): 
-            
-            estimators = [individual_models[source][i] for i in individual_models[source] if i not in skip_models] 
-            
+        for j,source in enumerate(potential_sources):
+
+            estimators = [individual_models[source][i] for i in individual_models[source] if i not in skip_models]
+
             X_train ,y_train = X_test_list[source],y_test_list[source]
             results[source] = train_and_test(networks[j],estimators,X_train,y_train,X_test,y_test)
-        
+
     ########## Networks built from concatenated data
     if 'concat' in include_est:
         estimators = [concat_models[target][i] for i in concat_models[target] if i not in skip_models]
         results['concat'] = train_and_test(networks[-1],estimators,X_train_concat,y_train_concat,X_test,y_test)
-    
+
     ########## Networks built from all individual models
     if 'all' in include_est:
-        estimators = [] 
+        estimators = []
         for source in potential_sources :
             estimators.extend([individual_models[source][i] for i in individual_models[source] if i not in skip_models])
         results['all'] = train_and_test(networks[-2],estimators,X_train_concat,y_train_concat,X_test,y_test)
@@ -100,25 +101,25 @@ def cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test
 
 '''
 Apply cycle_train_and_test for each possible target as defined in DataSets
-return dictionnary of results 
+return dictionnary of results
 results[target][source]
 '''
 
 def test_models_pb(individual_models,concat_models,DataSets,X_test_list,y_test_list,X_train_list,y_train_list,folder,network_name,include_est,skip_models=[]):
-    
+
     results ={}
-    
+
     for target in DataSets:
         print "# Target = ",target
-        
+
         X_test = X_test_list[target]
         y_test = y_test_list[target]
-        
+
         network = load_net(folder,network_name)
         result_target = cycle_train_and_test(individual_models,concat_models,DataSets,network,X_test_list,y_test_list,include_est,skip_models,target)
-        
+
         results[target] = result_target
-        
+
     return results
 
 
@@ -126,7 +127,7 @@ def test_models_pb(individual_models,concat_models,DataSets,X_test_list,y_test_l
 
 
 '''
-Test all models (All pb + networks + target) 
+Test all models (All pb + networks + target)
 
 '''
 
@@ -134,30 +135,30 @@ def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structu
 
     if DataSets == []:
         DataSets = range(len(feat_dic_list))
-    
+
     results = {}
-    
+
     def test_models_pb_aux(individual_models,feat_dic_list,featids_feat,structure_folder,networks,concat_folder,include_est,pb):
-        
+
         print "########### Problem = ",pb
 
         feat_weekids=[pb[1]]
         label_weekid=pb[0]
-        
+
         train_ratio=0.7
         X_train_list,X_test_list,y_train_list,y_test_list=prepare_problem_data_all(feat_dic_list,featids_feat,feat_weekids,label_weekid,train_ratio)
-        
+
         individual_models_pb = individual_models[pb]
         concat_models_pb = pickle.load(open(concat_folder+"concat_model_("+str(pb[0])+", "+str(pb[1])+").p","rb"))
-        
+
         result_pb = {}
         for network_name in networks:
             print "###### Testing network : "+network_name
             result_pb[network_name] = test_models_pb(individual_models_pb,concat_models_pb,DataSets,X_test_list,y_test_list,X_train_list,y_train_list,structure_folder,network_name,include_est,skip_models)
-            
+
         return result_pb
-    
-    
+
+
     ##### Fill results
     if para==1:
         partial_test_models_pb = partial(test_models_pb,individual_models,feat_dic_list,featids_feat,structure_folder,networks,concat_folder,pb)
@@ -171,17 +172,17 @@ def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structu
         results = {}
         for i,estimator_dic in enumerate(results_list):
             results[pb_list[i]] = estimator_dic
-    
+
     return results
 
-    
+
 
 
 # '''
-# Transform a results dictionnary of the form 
+# Transform a results dictionnary of the form
 # results[pb][target][source][layer][estimator] (dictionnary)
 # into a set of 3 lists :
-# - scores 
+# - scores
 # - indexes
 # - colors
 # for display and comparison purposes
@@ -212,7 +213,7 @@ def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structu
 #                     ind+=1
 #                     color_ind+=1
 #             ind+=1
-            
+
 #     #### Add Network Full AUC
 #     ind+=1
 #     scores.append(results_exp['all']['Layer 1']['Estimator 0'])
@@ -224,22 +225,22 @@ def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structu
 #     ind+=2
 #     scores.append(results_exp['all']['Layer 1']['Estimator 0'])
 #     index.append(ind)
-#     colors.append(c3) 
+#     colors.append(c3)
 
 #     #### Add Network '2L_LR'
 #     results_exp = results_networks['2L_simple'][target]
 #     ind+=2
 #     scores.append(results_exp['all']['Layer 2']['Estimator 0'])
 #     index.append(ind)
-#     colors.append(c3)   
+#     colors.append(c3)
 
 #     #### Add Network '2L_LR'
 #     results_exp = results_networks['2L_LR'][target]
 #     ind+=2
 #     scores.append(results_exp['all']['Layer 2']['Estimator 0'])
 #     index.append(ind)
-#     colors.append(c3)  
-      
+#     colors.append(c3)
+
 #     ## Formatting into pandas.DF
 #     pretty_results ={'Scores':scores,'Index':index,'Color':colors}
 #     data=pd.DataFrame.from_dict(pretty_results)
@@ -260,15 +261,3 @@ def test_all_models(individual_models,feat_dic_list,featids_feat,pb_list,structu
 #     xlab(xlabel) + ylab('ROC-AUC score')
 
 #     return plot
-
-
-
-
-
-
-
-
-
-
-
-
